1be9fb053866b7efc0bd5ff7eeee89f93b2b0e8c
ct-clmsn <ct.clmsn@gmail.com>
2021-05-22 23:35:23 -0400

    Update AppTest.java

--- src/test/java/org/jmq/collectives/AppTest.java
+++ src/test/java/org/jmq/collectives/AppTest.java
@@ -1,4 +1,4 @@
-//  Copyrig://github.com/ct-clmsn/jmq-collectives.gitt (c) 2021 Christopher Taylor
+//  Copyright (c) 2021 Christopher Taylor
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
