13ba0b41db058702faf605cfa562fc098904f939
ct.clmsn <ct_clmsn_at_gmail_dot_com>
2021-05-27 22:48:07 -0400

    added a BasicParams for BasicTcpBackend

--- src/main/java/org/jmq/collectives/Params.java
+++ src/main/java/org/jmq/collectives/Params.java
@@ -9,14 +9,14 @@ import java.lang.String;
 import java.util.Vector;
 import java.util.Map;
 
-public class Params {
+public class BasicParams {
 
     private long nranks_;
     private long rank_;
     private Vector<String> addresses_;
 
     public Params() {
-        Map<String, String> env = System.getenv();
+        final Map<String, String> env = System.getenv();
 
         if(env.containsKey("JMQ_COLLECTIVES_NRANKS")) {
             this.nranks_ = Long.parseLong(env.get("JMQ_COLLECTIVES_NRANKS"));
@@ -45,4 +45,54 @@ public class Params {
     public long rank() { return this.rank_; }
     
     public Vector<String> addresses() { return this.addresses_; }
+}
+
+public class Params extends BasicParams {
+
+    private long liveness;
+    private long interval;
+    private long interval_init;
+    private long interval_max;
+
+    public Params() {
+        super();
+
+        final Map<String, String> env = System.getenv();
+
+        if(env.containsKey("JMQ_COLLECTIVES_LIVENESS")) {
+            this.liveness = Long.parseLong(env.get("JMQ_COLLECTIVES_LIVENESS"));
+        }
+        else {
+            this.liveness = 0;
+        }
+
+        if(env.containsKey("JMQ_COLLECTIVES_INTERVAL")) {
+            this.interval = Long.parseLong(env.get("JMQ_COLLECTIVES_INTERVAL"));
+        }
+        else {
+            this.interval = 0;
+        }
+
+        if(env.containsKey("JMQ_COLLECTIVES_INTERVAL_MAX")) {
+            this.interval_max = Long.parseLong(env.get("JMQ_COLLECTIVES_INTERVAL_MAX"));
+        }
+        else {
+            this.interval_max = 0;
+        }
+
+        if(env.containsKey("JMQ_COLLECTIVES_INTERVAL_INIT")) {
+            this.interval_init = Long.parseLong(env.get("JMQ_COLLECTIVES_INTERVAL_INIT"));
+        }
+        else {
+            this.interval_init = 0;
+        }
+    }
+
+    public long getLiveness() { return this.liveness; }
+
+    public long getInterval() { return this.interval; }
+
+    public long getIntervalMax() { return this.interval_max; }
+
+    public long getIntervalInit() { return this.interval_init; }
 }
