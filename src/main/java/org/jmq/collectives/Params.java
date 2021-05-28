ab9ad89fe2208434c973f8aca0e01facc316e737
ct.clmsn <ct_clmsn_at_gmail_dot_com>
2021-05-27 22:49:08 -0400

    added BasicParams as a separate file

--- src/main/java/org/jmq/collectives/Params.java
+++ src/main/java/org/jmq/collectives/Params.java
@@ -9,44 +9,6 @@ import java.lang.String;
 import java.util.Vector;
 import java.util.Map;
 
-public class BasicParams {
-
-    private long nranks_;
-    private long rank_;
-    private Vector<String> addresses_;
-
-    public Params() {
-        final Map<String, String> env = System.getenv();
-
-        if(env.containsKey("JMQ_COLLECTIVES_NRANKS")) {
-            this.nranks_ = Long.parseLong(env.get("JMQ_COLLECTIVES_NRANKS"));
-        }
-        else {
-            this.nranks_ = 0;
-        }
-
-        if(env.containsKey("JMQ_COLLECTIVES_RANK")) {
-            this.rank_ = Long.parseLong(env.get("JMQ_COLLECTIVES_RANK"));
-        }
-        else {
-            this.rank_ = 0;
-        }
-
-        this.addresses_ = new Vector<String>();
-        if(env.containsKey("JMQ_COLLECTIVES_IPADDRESSES")) {
-            for(String addr : env.get("JMQ_COLLECTIVES_IPADDRESSES").split(",")) {
-                this.addresses_.addElement(addr);
-            }
-        }
-    }
-
-    public long n_ranks() { return this.nranks_; }
-
-    public long rank() { return this.rank_; }
-    
-    public Vector<String> addresses() { return this.addresses_; }
-}
-
 public class Params extends BasicParams {
 
     private long liveness;
